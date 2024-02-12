0d0d649b2599bba3cad170fc25e8f233b2dc1684
iheartwifi <wanlanhe@gmail.com>
2024-02-12 13:48:11 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matpilot.pyplot as plt
+import seaborn as
+
+%matplotlib inline
+sns.set_style("whitegrid")
+plt.sytle.us("fivethirtyeight")
+
+# example text for model training (SMS messages)
+simple_train = ['call you tonight', 'Call me a cab', 'Please call me...PLEASE!']
+
